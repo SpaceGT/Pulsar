83091d8323cca8f790d1ca1b9ed1dd4de694632c
Space <unclearspace@gmail.com>
2025-06-14 20:55:38 +0100

    Exit instantly

---
+++ PluginLoader/Patch/Patch_ExitThreadSafe.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using HarmonyLib;
+using Sandbox;
+
+namespace avaness.PluginLoader.Patch
+{
+    [HarmonyPatch(typeof(MySandboxGame), "ExitThreadSafe")]
+    public class Patch_ExitThreadSafe
+    {
+        public static bool Prefix()
+        {
+            Process.GetCurrentProcess().Kill();
+            return false;
+        }
+    }
+}
