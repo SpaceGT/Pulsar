b17c514a8af21dc2d67a45d5d6e041a8d92e1fbd
Space <unclearspace@gmail.com>
2025-09-13 11:22:30 +0100

    Harden updater

--- Updater/Writer.cs
+++ Updater/Writer.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Reflection;
+using System.Windows.Forms;
 
 namespace Pulsar.Updater;
 
 internal static class Writer
 {
+    private const string Pulsar = "Pulsar";
+    private const int MaxFiles = 15;
+
+    private static readonly HashSet<string> Preserve = ["Legacy", "Modern"];
+    private static readonly HashSet<string> Check = ["Legacy.exe", "Modern.exe", "LICENSE"];
+
     public static void Update(ZipArchive source, string destination)
     {
-        CleanFolder(destination, ["Legacy", "Modern"]);
+        if (!Validate(destination))
+            Environment.Exit(1);
+
+        CleanFolder(destination, Preserve);
         source.ExtractToDirectory(destination);
     }
 
@@ -32,5 +44,45 @@ internal static class Writer
     {
         string updaterFolder = Path.GetDirectoryName(updater);
         return Path.GetFullPath(updaterFolder) == Path.GetFullPath(folder);
+    }
+
+    private static bool Validate(string folder)
+    {
+        if (!Directory.Exists(folder))
+            return false;
+
+        folder = Path.GetFullPath(folder);
+
+        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string defaultPath = Path.Combine(appData, Pulsar);
+
+        if (folder == defaultPath)
+            return true;
+
+        bool isPulsarInstall = Check.All(name => File.Exists(Path.Combine(folder, name)));
+        bool hasOtherFiles = Directory.GetFiles(folder).Length > MaxFiles;
+        if (isPulsarInstall && !hasOtherFiles)
+            return true;
+
+        return ContinuePrompt(folder);
+    }
+
+    private static bool ContinuePrompt(string folder)
+    {
+        string caption = "Pulsar Updater";
+        string message =
+            "The installation folder could not be validated!\n"
+            + "Is this your Pulsar install folder?\n"
+            + "It WILL BE CLEANED if you update!\n\n"
+            + folder;
+
+        DialogResult response = MessageBox.Show(
+            message,
+            caption,
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Warning
+        );
+
+        return response == DialogResult.Yes;
     }
 }
