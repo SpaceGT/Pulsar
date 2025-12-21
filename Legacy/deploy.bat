e9398148cde7262ac5707c93906855a36b1c24ec
Space <unclearspace@gmail.com>
2025-12-21 22:40:13 +0000

    Bump ProtoBuf version

--- Legacy/deploy.bat
+++ Legacy/deploy.bat
@@ -108,6 +108,7 @@ copy /y /b "%SOURCE%\NLog.dll" "%LIBRARY_DIR%\" >NUL 2>&1
 
 echo Copying "protobuf-net.dll"
 copy /y /b "%SOURCE%\protobuf-net.dll" "%LIBRARY_DIR%\" >NUL 2>&1
+copy /y /b "%SOURCE%\protobuf-net.Core.dll" "%LIBRARY_DIR%\" >NUL 2>&1
 
 echo Copying "FuzzySharp.dll"
 copy /y /b "%SOURCE%\FuzzySharp.dll" "%LIBRARY_DIR%\" >NUL 2>&1
