9f51c24e9a23e429d2ce9959bf27e9d64811d92a
WesternSpace <80211714+WesternSpace@users.noreply.github.com>
2026-01-22 16:21:25 -0500

    Initial implementation of Github plugin and local folder plugin controls for plugin details screen.

---
+++ Modern/Screens/PluginDetailsScreen/Controls/LocalPluginFolderControls.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Pulsar.Modern.Screens.PluginDetailsScreen.Controls;
+
+public partial class LocalPluginFolderControls : UserControl
+{
+    public LocalPluginFolderControls()
+    {
+        InitializeComponent();
+    }
+}
