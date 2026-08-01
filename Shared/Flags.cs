4253e53069435c29147107192bf33b348bb93cd7
Space <unclearspace@gmail.com>
2026-08-01 13:56:30 +0100

    Cross-platform flags

--- Shared/Flags.cs
+++ Shared/Flags.cs
@@ -33,28 +33,28 @@ public static class Flags
 
     static Flags()
     {
-        if (HasArg("nosplash"))
+        if (HasArg("no", "splash"))
             SplashType = SplashType.None;
-        else if (HasArg("sesplash"))
+        else if (HasArg("se", "splash"))
             SplashType = SplashType.Native;
         else
             SplashType = SplashType.Pulsar;
 
         // Linux updater will come in a future pass
-        if (HasArg("noupdate") || !Tools.IsWindows())
+        if (HasArg("no", "update") || !Tools.IsWindows())
             UpdateType = UpdateType.None;
-        else if (HasArg("prerelease"))
+        else if (HasArg("pre", "release"))
             UpdateType = UpdateType.Tester;
         else
             UpdateType = UpdateType.Standard;
 
         ExternalDebug = HasArg("debug");
-        DebugMenu = HasArg("f12menu");
+        DebugMenu = HasArg("f12", "menu");
         CustomSources = HasArg("sources");
         ContinueGame = HasArg("continue");
-        CheckAllPlugins = HasArg("debugCompileAll");
-        GameIntroVideo = HasArg("keepintro");
-        MakeCheckFile = HasArg("mkcheck");
+        CheckAllPlugins = HasArg("debug", "compile", "all");
+        GameIntroVideo = HasArg("keep", "intro");
+        MakeCheckFile = HasArg("mk", "check");
         TrustedMods = HasArg("hardened");
     }
 
@@ -93,8 +93,17 @@ public static class Flags
             LogFile.WriteLine($"Enabled flags: {string.Join(" ", changed)}");
     }
 
-    private static bool HasArg(string argument) =>
-        Environment
-            .GetCommandLineArgs()
-            .Any(arg => arg.Equals($"-{argument}", StringComparison.OrdinalIgnoreCase));
+    private static bool HasArg(params string[] words)
+    {
+        string joined = string.Join("", words);
+        string unix = $"--{string.Join("-", words)}";
+        string windows = $"/{joined}";
+        string spaceEngineers = $"-{joined}";
+
+        return Environment.GetCommandLineArgs().Any(arg =>
+            arg.Equals(unix, StringComparison.OrdinalIgnoreCase)
+            || arg.Equals(windows, StringComparison.OrdinalIgnoreCase)
+            || arg.Equals(spaceEngineers, StringComparison.OrdinalIgnoreCase)
+        );
+    }
 }
