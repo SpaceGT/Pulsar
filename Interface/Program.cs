7db817d3bf13e25ebd99a91b0123575527eaed7c
Space <unclearspace@gmail.com>
2026-08-29 17:50:48 +0100

    Fix UI on proton

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -5,9 +5,18 @@ namespace Pulsar.Interface;
 
 internal static class Program
 {
+    private const string BuiltInComInterop =
+        "System.Runtime.InteropServices.BuiltInComInterop.IsSupported";
+
     [STAThread]
     public static void Main(string[] args)
     {
+#if NETFRAMEWORK
+        // Mono's ConditionalWeakTable crashes Avalonia's Win32 automation.
+        if (Type.GetType("Mono.Runtime") is not null)
+            AppDomain.CurrentDomain.SetData(BuiltInComInterop, "false");
+#endif
+
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
